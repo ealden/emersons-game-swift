6d93850f7e5c162972226ffe4983b2db97b7705c
Ealden Esto E. Escañan <ealden@escanan.com>
2020-05-10 14:23:32 +0800

    Initial Commit

---
+++ EmersonsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmersonsGame
+//
+//  Created by Ealden Escañan on 5/10/20.
+//  Copyright © 2020 Ealden Escañan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
